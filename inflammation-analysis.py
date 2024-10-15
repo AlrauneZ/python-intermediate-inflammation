b8a5097e344cca30f078a8be5ab142e619a87e6e
Alraune Zech <a.zech@uu.nl>
2024-10-15 11:36:12 +0200

    modify function on std by renaming, return value, way of calculating values; also adapting its call in controler

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -21,9 +21,17 @@ def main(args):
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data), **(models.s_dev(inflammation_data))}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data),
+                     'standard deviation': models.daily_standard_deviation(inflammation_data),
+                     }
 
         views.visualize(view_data)
+
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
